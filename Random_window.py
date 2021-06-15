daa50e2998cd4957da097b5103e301f9beb0d244
zaid070902 <zaidflandorp4@gmail.com>
2021-06-15 16:35:08 +0200

    Update

--- Random_window.py
+++ Random_window.py
@@ -1,9 +1,82 @@
-import random
 from tkinter import *
+import random
+from tkinter import messagebox
 
 root = Tk()
-root.geometry("600x500")
+root.geometry("650x600")
 root.config(bg="#111")
 
 
+class Play:
+    def __init__(self):
+        self.canvas = Canvas(root, width=300, height=100)
+        self.canvas.place(x=190, y=30)
+        self.img = PhotoImage(file="img_1.png")
+        self.canvas.create_image(0, 0, anchor=NW, image=self.img)
+
+        self.num_select1 = Button(root, width=2, bg="gold").place(x=50, y=200)
+        self.num_select2 = Button(root, width=2, bg="gold").place(x=100, y=200)
+        self.num_select3 = Button(root, width=2, bg="gold").place(x=150, y=200)
+        self.num_select4 = Button(root, width=2, bg="gold").place(x=200, y=200)
+        self.num_select5 = Button(root, width=2, bg="gold").place(x=250, y=200)
+        self.num_select7 = Button(root, width=2, bg="gold").place(x=300, y=200)
+        self.num_select8 = Button(root, width=2, bg="gold").place(x=350, y=200)
+        self.num_select9 = Button(root, width=2, bg="gold").place(x=50, y=250)
+        self.num_select10 = Button(root, width=2, bg="gold").place(x=100, y=250)
+        self.num_select11 = Button(root, width=2, bg="gold").place(x=150, y=250)
+        self.num_select12 = Button(root, width=2, bg="gold").place(x=200, y=250)
+        self.num_select13 = Button(root, width=2, bg="gold").place(x=250, y=250)
+        self.num_select14 = Button(root, width=2, bg="gold").place(x=300, y=250)
+        self.num_select15 = Button(root, width=2, bg="gold").place(x=350, y=250)
+        self.num_select16 = Button(root, width=2, bg="gold").place(x=50, y=300)
+        self.num_select17 = Button(root, width=2, bg="gold").place(x=100, y=300)
+        self.num_select18 = Button(root, width=2, bg="gold").place(x=150, y=300)
+        self.num_select19 = Button(root, width=2, bg="gold").place(x=200, y=300)
+        self.num_select20 = Button(root, width=2, bg="gold").place(x=250, y=300)
+        self.num_select21 = Button(root, width=2, bg="gold").place(x=300, y=300)
+        self.num_select22 = Button(root, width=2, bg="gold").place(x=350, y=300)
+        self.num_select23 = Button(root, width=2, bg="gold").place(x=50, y=350)
+        self.num_select24 = Button(root, width=2, bg="gold").place(x=100, y=350)
+        self.num_select25 = Button(root, width=2, bg="gold").place(x=150, y=350)
+        self.num_select26 = Button(root, width=2, bg="gold").place(x=200, y=350)
+        self.num_select27 = Button(root, width=2, bg="gold").place(x=250, y=350)
+        self.num_select28 = Button(root, width=2, bg="gold").place(x=300, y=350)
+        self.num_select29 = Button(root, width=2, bg="gold").place(x=350, y=350)
+        self.num_select30 = Button(root, width=2, bg="gold").place(x=50, y=400)
+        self.num_select31 = Button(root, width=2, bg="gold").place(x=100, y=400)
+        self.num_select32 = Button(root, width=2, bg="gold").place(x=150, y=400)
+        self.num_select33 = Button(root, width=2, bg="gold").place(x=200, y=400)
+        self.num_select34 = Button(root, width=2, bg="gold").place(x=250, y=400)
+        self.num_select35 = Button(root, width=2, bg="gold").place(x=300, y=400)
+        self.num_select36 = Button(root, width=2, bg="gold").place(x=350, y=400)
+        self.num_select37 = Button(root, width=2, bg="gold").place(x=50, y=450)
+        self.num_select38 = Button(root, width=2, bg="gold").place(x=100, y=450)
+        self.num_select39 = Button(root, width=2, bg="gold").place(x=150, y=450)
+        self.num_select40 = Button(root, width=2, bg="gold").place(x=200, y=450)
+        self.num_select41 = Button(root, width=2, bg="gold").place(x=250, y=450)
+        self.num_select42 = Button(root, width=2, bg="gold").place(x=300, y=450)
+        self.num_select43 = Button(root, width=2, bg="gold").place(x=350, y=450)
+        self.num_select44 = Button(root, width=2, bg="gold").place(x=50, y=500)
+        self.num_select45 = Button(root, width=2, bg="gold").place(x=100, y=500)
+        self.num_select46 = Button(root, width=2, bg="gold").place(x=150, y=500)
+        self.num_select47 = Button(root, width=2, bg="gold").place(x=200, y=500)
+        self.num_select48 = Button(root, width=2, bg="gold").place(x=250, y=500)
+        self.num_select49 = Button(root, width=2, bg="gold").place(x=300, y=500)
+        self.num_select50 = Button(root, width=2, bg="gold").place(x=350, y=500)
+
+
+
+
+
+
+
+        x = random.sample(range(1, 49), 6)
+        print(x)
+
+
+Play()
+
 root.mainloop()
+
+
+x = open("File.txt", 'w')
--- main.py
+++ main.py
@@ -2,35 +2,37 @@ from tkinter import *
 import rsaidnumber
 from datetime import datetime, timedelta
 from tkinter import messagebox
+from email_validator import validate_email, EmailNotValidError
 
 master = Tk()
 master.geometry("600x500")
 master.config(bg="#111")
+
 
 canvas = Canvas(master, width=300, height=100)
 canvas.place(x=150, y=10)
 img = PhotoImage(file="img_1.png")
 canvas.create_image(0, 0, anchor=NW, image=img)
 
-name_lab = Label(master, text="Enter name:", bg="#111", fg="white", font=15)
+name_lab = Label(master, text="Enter name:", bg="#111", fg="gold", font=15)
 name_lab.place(x=100, y=150)
 
 name_ent = Entry(master)
 name_ent.place(x=300, y=150)
 
-email_lab = Label(master, text="Enter email address:", bg="#111", fg="white", font=15)
+email_lab = Label(master, text="Enter email address:", bg="#111", fg="gold", font=15)
 email_lab.place(x=100, y=200)
 
 email_ent = Entry(master)
 email_ent.place(x=300, y=200)
 
-id_num_lab = Label(master, text="Enter ID number:", bg="#111", fg="white", font=15)
+id_num_lab = Label(master, text="Enter ID number:", bg="#111", fg="gold", font=15)
 id_num_lab.place(x=100, y=250)
 
 id_num_ent = Entry(master)
 id_num_ent.place(x=300, y=250)
 
-address_lab = Label(master, text="Enter address:", bg="#111", fg="white", font=15)
+address_lab = Label(master, text="Enter address:", bg="#111", fg="gold", font=15)
 address_lab.place(x=100, y=300)
 
 address_ent = Entry(master)
@@ -38,15 +40,17 @@ address_ent.place(x=300, y=300)
 
 
 def sub():
-    var = ""
-    my_file = open("Text_file.txt", 'w')
-    var += name_ent.get()
-    var += '\n'
-    var += email_ent.get()
-    var += '\n'
-    var += address_ent.get()
-    my_file.write(var)
-    # import Random_window
+    text = ""
+    my_file = open("Text_file.txt", 'a')
+    text += "Username: " + name_ent.get()
+    text += '\n'
+    text += "Email: " + email_ent.get()
+    text += '\n'
+    text += "Address: " + address_ent.get()
+    text += '\n'
+    text += "ID number: " + id_num_ent.get()
+    text += '\n'
+    my_file.write(text)
 
     id_num = rsaidnumber.parse(id_num_ent.get())
     age = ((datetime.today() - id_num.date_of_birth) // timedelta(days=365.25))
@@ -54,10 +58,20 @@ def sub():
         messagebox.showinfo("Let's Play")
     else:
         years_left = 18 - age
-        messagebox.showinfo("Return in", str(years_left) + "years time")
+        messagebox.showinfo("Error", "Return in" + '\n' + str(years_left) + "years time")
+
+    try:
+        # Validate.
+        validate_email(email_ent.get())
+        # Update with the normalized form.
+    except EmailNotValidError:
+        # email is not valid
+        messagebox.showinfo("Invalid ID number. Try again")
+    master.destroy()
+    import Random_window
 
 
-sub_btn = Button(master, text="Submit", width=10, height=2, bg="#111", fg="white", font=15, command=sub)
+sub_btn = Button(master, text="Submit", width=10, height=2, bg="#111", fg="gold", font=15, command=sub)
 sub_btn.place(x=230, y=350)
 
 
